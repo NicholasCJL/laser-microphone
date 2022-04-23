a1c76a3ae8668b945bba4481f8d52f582752a645
Nicholas C <chong.jiale.nicholas@gmail.com>
2022-04-23 10:11:16 +0800

    Changed plot scale

--- background_noise.py
+++ background_noise.py
@@ -3,7 +3,8 @@ from utility_functions import *
 in_path = "110322"
 filename = "C4background0000"
 out_path = "audio"
-target_freq = 5000
+
+c_list = ['r', 'g', 'b']
 
 for i in range(3):
     data = subtract_mean(get_data(f"{in_path}/{filename}{i}.csv"))
@@ -15,8 +16,8 @@ for i in range(3):
         y_total += y
     print(f.size)
     fig, ax = plt.subplots()
-    plt.plot(f[1:], np.log10(np.abs(y[1:]) ** 2), c='k', linewidth=0.5)
-    plt.xlabel("Frequency", fontsize=16)
+    plt.plot(np.log10(f[1:]), 10 * np.log10(np.abs(y[1:]) ** 2), c=c_list[i], linewidth=0.5)
+    plt.xlabel("log Frequency", fontsize=16)
     plt.ylabel("Power (dB)", fontsize=16)
     ax.tick_params(axis='both', labelsize=15)
     fig.set_size_inches(14, 10)
@@ -25,8 +26,8 @@ for i in range(3):
     sf.write(f"{out_path}/background_original{i}.wav", data, 20000)
 
 fig, ax = plt.subplots()
-plt.plot(f[1:], np.log10(np.abs(y_total[1:] / 3) ** 2), linewidth=0.5)
-plt.xlabel("Frequency", fontsize=16)
+plt.plot(np.log10(f[1:]), 10 * np.log10(np.abs(y_total[1:] / 3) ** 2), c='k', linewidth=0.5)
+plt.xlabel("log Frequency", fontsize=16)
 plt.ylabel("Power (dB)", fontsize=16)
 ax.tick_params(axis='both', labelsize=15)
 fig.set_size_inches(14, 10)
